00c430c28dd39e77780d5341ea231d5bd0ceaf15
Jean-Francois Beaulac <jeanfrancoisbeaulac@hotmail.com>
2021-08-24 09:23:02 -0400

    add some sample jpa query methods to the project

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,12 +1,33 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> category = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByUom("Teaspoon");
+
+        System.out.println("Cat Id is: " + category.get().getId());
+        System.out.println("UOM Id is: " + uom.get().getId());
+
         return "index";
     }
 }
